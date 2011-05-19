0973e034be47996319ead4be8d7a318765ee048d
ghy <ghy@you-may.cn>
2011-05-19 14:59:53 +0800

    2008.sln 和2010.sln二个解决方案文件

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services;
+
+namespace Web.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!" + Logic.GetName();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
